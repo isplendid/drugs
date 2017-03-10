e5377269a9b86c6174845ceed5a6e0835346b8a5
Zoltan Kollar <kolla.zoltan@gmail.com>
2017-03-10 13:04:50 +0100

    SOAP

--- app/Module.scala
+++ app/Module.scala
@@ -1,11 +1,22 @@
 import com.google.inject.AbstractModule
 import dao._
 import net.codingwell.scalaguice.ScalaModule
+import org.apache.cxf.transport.play.CtxConfigurator
+import org.springframework.context.support.ClassPathXmlApplicationContext
 import services.drug.{DrugDBService, DrugService}
 
 class Module extends AbstractModule with ScalaModule {
 
   override def configure() = {
+    val ctx = new ClassPathXmlApplicationContext("applicationContext.xml")
+
+    bind(classOf[ClassPathXmlApplicationContext])
+      .toInstance(ctx)
+
+    val DrugsSoapController = ctx.getBean("DrugsServiceImplBean")
+    requestInjection(DrugsSoapController)
+
+    bind[CtxConfigurator].to[SoapConfigurator]
 
     bind[DistributorDAO].to[DistributorSlickDAO]
     bind[DrugDAO].to[DrugSlickDAO]
--- app/util/Util.scala
+++ app/util/Util.scala
@@ -2,7 +2,8 @@ package util
 
 import com.google.inject.Singleton
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 @Singleton
 class Util {
@@ -10,6 +11,8 @@ class Util {
 }
 
 object Util {
+
+  def AwaitResult[A](f: => Future[A]): A = Await.result(f, 1.seconds)
 
   object Implicits {
 
